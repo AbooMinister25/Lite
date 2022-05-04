b873106eb2404fc64d71e1d6eec21b322a3c5305
AbooMinister25 <aboominister@gmail.com>
2022-05-04 18:47:00 -0400

    Finished parsing for prefix expression. Reworked get_precedence method in crates/parser/precedence.rs to return u8s instead of the Precedence struct

--- crates/parser/src/expression.rs
+++ crates/parser/src/expression.rs
@@ -232,6 +232,8 @@ impl<'a> Parser<'a> {
             TokenKind::Ident(_) => Some(self.parse_ident(token)),
             TokenKind::OpenParen => Some(self.parse_grouping()),
             TokenKind::Minus => Some(self.parse_unary(token)),
+            TokenKind::Bang => Some(self.parse_unary(token)),
+            TokenKind::OpenBracket => Some(self.parse_array(token)),
             _ => {
                 parser_error(
                     &format!("Invalid Syntax - Expected expression, found {}", token.0),
@@ -240,6 +242,24 @@ impl<'a> Parser<'a> {
                 );
                 None
             }
+        }
+    }
+
+    fn infix_rule(&mut self, lhs: Spanned<Expr>) -> Result<Spanned<Expr>, ()> {
+        match self.peek().0 {
+            TokenKind::Plus
+            | TokenKind::Minus
+            | TokenKind::Star
+            | TokenKind::Slash
+            | TokenKind::EqualEqual
+            | TokenKind::BangEqual
+            | TokenKind::Greater
+            | TokenKind::GreaterEqual
+            | TokenKind::Less
+            | TokenKind::LessEqual
+            | TokenKind::And
+            | TokenKind::Or => todo!(),
+            _ => todo!(),
         }
     }
 
@@ -258,9 +278,12 @@ impl<'a> Parser<'a> {
     /// let expr = parser.parse_expression().unwrap();
     pub fn parse_expression(&mut self, precedence: u8) -> Result<Spanned<Expr>, ()> {
         let token = self.advance();
-        let kind = &token.0;
 
-        if let Some(lhs) = self.prefix_rule(token) {
+        if let Some(mut lhs) = self.prefix_rule(token) {
+            while precedence < get_precedence(&self.peek().0) {
+                lhs = self.infix_rule(lhs?);
+            }
+
             return lhs;
         }
 
@@ -347,6 +370,11 @@ impl<'a> Parser<'a> {
 
     fn parse_grouping(&mut self) -> Result<Spanned<Expr>, ()> {
         let expr = self.parse_expression(1)?;
+        // If next token is a comma, parse as a tuple
+        if self.peek().0 == TokenKind::Comma {
+            return self.parse_tuple(expr);
+        }
+
         self.consume(
             TokenKind::CloseParen,
             "Expected to find closing parenthesis `)`",
@@ -354,9 +382,58 @@ impl<'a> Parser<'a> {
         Ok(expr)
     }
 
+    fn parse_tuple(&mut self, first_value: Spanned<Expr>) -> Result<Spanned<Expr>, ()> {
+        let start = first_value.1.start; // Store start of span for later use
+        let mut items = vec![first_value];
+
+        while self.peek().0 != TokenKind::CloseParen {
+            let item = self.parse_expression(1)?;
+            items.push(item);
+
+            // Don't use `consume` since we don't want to error if there isn't a comma
+            if self.peek().0 == TokenKind::Comma {
+                self.advance();
+            }
+        }
+
+        self.consume(
+            TokenKind::CloseParen,
+            "Expected to find closing parenthesis `)`",
+        );
+
+        let span = start - 1..items.last().unwrap().1.end + 1; // Safe to unwrap since `items` has at least one value
+        Ok((Expr::Tuple(items), span))
+    }
+
+    fn parse_array(&mut self, current: Spanned<TokenKind>) -> Result<Spanned<Expr>, ()> {
+        let mut items = Vec::new();
+
+        while self.peek().0 != TokenKind::CloseBracket {
+            let item = self.parse_expression(1)?;
+            items.push(item);
+
+            // Don't use `consume` since we don't want to error if there isn't a comma
+            if self.peek().0 == TokenKind::Comma {
+                self.advance();
+            }
+        }
+
+        self.consume(
+            TokenKind::CloseBracket,
+            "Expected to find closing bracket `]`",
+        );
+
+        let span = if items.is_empty() {
+            current.1.start..current.1.start + 1
+        } else {
+            current.1.start..items.last().unwrap().1.end // Safe to unwrap since `args` is confirmed to not be empty
+        };
+
+        Ok((Expr::Array(items), span))
+    }
+
     fn parse_unary(&mut self, current: Spanned<TokenKind>) -> Result<Spanned<Expr>, ()> {
-        let precedence = get_precedence(&current.0);
-        let expr = self.parse_expression(precedence.prefix.unwrap())?;
+        let expr = self.parse_expression(8)?; // 8 is the precedence level for `!` and `-` as unary operators
 
         let span = current.1.start..expr.1.end;
 
--- crates/parser/src/precedence.rs
+++ crates/parser/src/precedence.rs
@@ -15,25 +15,21 @@ impl Precedence {
 }
 
 /// Returns the precedence value for a given token
-pub fn get_precedence(kind: &TokenKind) -> Precedence {
+pub fn get_precedence(kind: &TokenKind) -> u8 {
     match kind {
         // Assignment operators = += -= *= /=
         TokenKind::Equal
         | TokenKind::PlusEqual
         | TokenKind::MinusEqual
         | TokenKind::StarEqual
-        | TokenKind::SlashEqual => Precedence::new(None, Some(1)),
-        TokenKind::Or => Precedence::new(None, Some(2)),
-        TokenKind::And => Precedence::new(None, Some(3)),
-        TokenKind::EqualEqual | TokenKind::BangEqual => Precedence::new(None, Some(4)),
-        TokenKind::Greater | TokenKind::GreaterEqual | TokenKind::Less | TokenKind::LessEqual => {
-            Precedence::new(None, Some(5))
-        }
-        TokenKind::Plus => Precedence::new(None, Some(6)),
-        TokenKind::Minus => Precedence::new(Some(8), Some(6)),
-        TokenKind::Star | TokenKind::Slash => Precedence::new(None, Some(7)),
-        TokenKind::Bang => Precedence::new(Some(8), None),
-        TokenKind::OpenParen | TokenKind::Dot => Precedence::new(None, Some(9)),
-        _ => Precedence::new(None, Some(0)),
+        | TokenKind::SlashEqual => 1,
+        TokenKind::Or => 2,
+        TokenKind::And => 3,
+        TokenKind::EqualEqual | TokenKind::BangEqual => 4,
+        TokenKind::Greater | TokenKind::GreaterEqual | TokenKind::Less | TokenKind::LessEqual => 5,
+        TokenKind::Plus | TokenKind::Minus => 6,
+        TokenKind::Star | TokenKind::Slash => 7,
+        TokenKind::OpenParen | TokenKind::Dot => 9,
+        _ => 0,
     }
 }
