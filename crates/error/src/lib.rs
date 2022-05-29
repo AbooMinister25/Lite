e1685c4b134b6e1d948934cbb21162b6523e943c
AbooMinister25 <aboominister@gmail.com>
2022-05-29 11:08:24 -0400

    Set up LiteError trait for implementing error messages across the compiler. Implemented LiteError for the ParserError enum in the parser crate

---
+++ crates/error/src/lib.rs
@@ -0,0 +1,89 @@
+//! This crate contains tools related to error reporting in Lite. This is achieved through
+//! the `LiteError` trait which can be implemented in different parts of the parser, representing
+//! error types and information specific to that area of the parser. Anything that implements the
+//! `LiteError` trait can then be used to construct a report using the `ariadne` crate.
+
+use ariadne::{Color, Label, Report, ReportKind, Source};
+
+/// This trait can be implemented to provide an interface usable for
+/// creating reportss
+pub trait LiteError {
+    fn labels(&self) -> Vec<(String, std::ops::Range<usize>)>;
+    fn message(&self) -> String;
+    fn kind(&self) -> ReportKind;
+    fn help(&self) -> Option<String> {
+        None
+    }
+    fn note(&self) -> Option<String> {
+        None
+    }
+    fn color(&self) -> Color {
+        match self.kind() {
+            ReportKind::Error => Color::Red,
+            ReportKind::Warning => Color::Yellow,
+            ReportKind::Advice => Color::Cyan,
+            ReportKind::Custom(_, color) => color,
+        }
+    }
+    fn build_report(&self) -> ariadne::Report {
+        let offset = &self.labels().iter().fold(
+            0,
+            |acc, (_, span)| if span.start > acc { span.start } else { acc },
+        );
+        let mut report = Report::build(self.kind(), (), *offset).with_message(self.message());
+
+        report.add_labels(
+            self.labels()
+                .iter()
+                .map(|(label, span)| Label::new(span.start..span.end).with_message(label)),
+        );
+
+        if let Some(note) = self.note() {
+            report.set_note(note)
+        }
+
+        if let Some(help) = self.help() {
+            report.set_help(help)
+        }
+
+        report.finish()
+    }
+}
+
+/// This struct holds a vec of errors to be reported by the parser
+pub struct Reporter<'a> {
+    reports: Vec<Box<dyn LiteError>>,
+    source: &'a str,
+}
+
+impl<'a> Reporter<'a> {
+    /// Constructs a new `Reporter` from a vec of errors and a source string
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// let reporter = Reporter::new(vec![], "5 + 5");
+    /// ```
+    pub fn new(reports: Vec<Box<dyn LiteError>>, source: &'a str) -> Self {
+        Self { reports, source }
+    }
+
+    /// Loops over all reports and prints them to the stderr
+    pub fn report(&self) -> Result<(), std::io::Error> {
+        for report in self.reports.iter() {
+            report.build_report().eprint(Source::from(self.source))?;
+        }
+
+        Ok(())
+    }
+
+    /// Adds a single report to the `Reporter`
+    pub fn add_report(&mut self, report: Box<dyn LiteError>) {
+        self.reports.push(report)
+    }
+
+    /// Extends the `Reporter` from a vec of reports
+    pub fn add_reports(&mut self, mut reports: Vec<Box<dyn LiteError>>) {
+        self.reports.append(&mut reports)
+    }
+}
--- crates/parser/src/errors.rs
+++ crates/parser/src/errors.rs
@@ -1,13 +1,13 @@
 use crate::ast::Spanned;
 use ariadne::{Color, ColorGenerator, Fmt, Label, Report, ReportBuilder, ReportKind, Source};
+use error::LiteError;
 use lexer::tokens::TokenKind;
-use std::fmt;
 use std::ops::Range;
 
 #[derive(Debug)]
 pub enum ParserError {
     /// Expected one of the given tokens, found something else
-    Expected(Vec<TokenKind>, Range<usize>),
+    Expected(Vec<TokenKind>, TokenKind, Range<usize>),
     /// An unclosed delimeter
     Unclosed(TokenKind, Range<usize>),
     /// Found an unexpected token
@@ -16,10 +16,10 @@ pub enum ParserError {
     Other(String, Range<usize>),
 }
 
-impl fmt::Display for ParserError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ParserError::Expected(expected, _) => {
+impl LiteError for ParserError {
+    fn labels(&self) -> Vec<(String, std::ops::Range<usize>)> {
+        let label = match self {
+            ParserError::Expected(expected, _, span) => {
                 let message = if expected.len() == 1 {
                     format!("Expected to find token {}", expected[0])
                 } else {
@@ -33,11 +33,46 @@ impl fmt::Display for ParserError {
                     )
                 };
 
-                write!(f, "{}", message)
+                (message, span.start..span.end)
             }
-            ParserError::Unclosed(token, _) => write!(f, "Unclosed delimiter {}", token),
-            ParserError::Unexpected(token, _) => write!(f, "Unexpected token {}", token),
-            ParserError::Other(message, _) => write!(f, "{}", message),
+            ParserError::Unclosed(token, span) => {
+                (format!("Unclosed delimiter {token}"), span.start..span.end)
+            }
+            ParserError::Unexpected(token, span) => {
+                (format!("Unexpected token {token}"), span.start..span.end)
+            }
+            ParserError::Other(message, span) => (message.to_string(), span.start..span.end),
+        };
+
+        vec![label]
+    }
+
+    fn message(&self) -> String {
+        match self {
+            ParserError::Expected(expected, found, _) => {
+                if expected.len() == 1 {
+                    format!(
+                        "Expected to find token {}, instead found {found}",
+                        expected[0]
+                    )
+                } else {
+                    format!(
+                        "Expected to find one of {:?}, instead found {found}",
+                        expected
+                            .iter()
+                            .map(|t| t.to_string())
+                            .collect::<Vec<String>>()
+                            .join(", ")
+                    )
+                }
+            }
+            ParserError::Unclosed(token, _) => format!("Unclosed delimiter {token}"),
+            ParserError::Unexpected(token, _) => format!("Unexpected token {token}"),
+            ParserError::Other(message, _) => message.to_string(),
         }
+    }
+
+    fn kind(&self) -> ReportKind {
+        ReportKind::Error
     }
 }
