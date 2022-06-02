c04027328f9eb67c85ece381e9d64d648535539c
AbooMinister25 <aboominister@gmail.com>
2022-06-02 19:27:36 -0400

    Added parsing for return statements

--- crates/parser/src/statement.rs
+++ crates/parser/src/statement.rs
@@ -5,10 +5,30 @@ use lexer::tokens::TokenKind;
 use span::{Span, Spanned};
 
 impl<'a> Parser<'a> {
+    /// Parses a statement
+    ///
+    /// # Examples
+    /// ```
+    /// use parser::Parser;
+    ///
+    /// let source = r#"
+    /// func main() do
+    ///     println("Hello, World")
+    /// end
+    /// "#;
+    ///
+    /// let mut parser = Parser::new(source, "main.lt");
+    /// let (statement, span) = parser.parse_statement().expect("Parser encountered an error");
+    /// ```
+    /// 
+    /// # Errors
+    /// This functions returns a `ParserError` if any errors is encountered
+    /// during parsing.
     pub fn parse_statement(&mut self) -> Result<Spanned<Statement>, ParserError> {
         let peeked = self.peek();
 
         match peeked.0 {
+            TokenKind::Return => self.parse_return(),
             _ => self.expression_statement(),
         }
     }
@@ -18,5 +38,13 @@ impl<'a> Parser<'a> {
         let span = Span::from(expr.1.start..expr.1.end);
         self.maybe_newline();
         Ok((Statement::Expression(expr), span))
+    }
+
+    fn parse_return(&mut self) -> Result<Spanned<Statement>, ParserError> {
+        let span_start = self.advance().1;
+        let expr = self.parse_expression(1)?;
+
+        let span = Span::from(span_start.start..expr.1.end);
+        Ok((Statement::Return(expr), span))
     }
 }
